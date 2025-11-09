9497073815db46fbb83fec28a3add25130548e49
Marina Kostova <116514172+marina9222@users.noreply.github.com>
2025-11-09 19:03:00 +0000

    Add: allowed hosts to accept all renders

--- guinea_pig_symptom_checker/settings.py
+++ guinea_pig_symptom_checker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "0") == "1"
-ALLOWED_HOSTS = ['guinea-pig-symptom-checker.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
