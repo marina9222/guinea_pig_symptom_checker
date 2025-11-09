dc8bfde95f5e2dbe96fda2f81c0ae9ec5428f853
Marina Kostova <116514172+marina9222@users.noreply.github.com>
2025-11-09 20:27:50 +0000

    remove : Remove load_data import and route

--- guinea_pig_symptom_checker/urls.py
+++ guinea_pig_symptom_checker/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from checker import views
-from checker.views import load_data
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
