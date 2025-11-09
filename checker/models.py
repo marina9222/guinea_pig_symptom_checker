5e3cac0ce5ea93aba70ccc188c99500e78b45c95
Marina Kostova <116514172+marina9222@users.noreply.github.com>
2025-11-09 16:27:30 +0000

    feature : create models and template for the home page

--- checker/models.py
+++ checker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Symptom(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Disease(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    symptoms = models.ManyToManyField(Symptom)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
