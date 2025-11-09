7568b534e172c0b4741a10ffeb328d11704749c4
Marina Kostova <116514172+marina9222@users.noreply.github.com>
2025-11-09 16:46:08 +0000

    ft: create a home page template , update models with adding url link

--- checker/models.py
+++ checker/models.py
@@ -10,8 +10,9 @@ class Symptom(models.Model):
 
 class Disease(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    symptoms = models.ManyToManyField(Symptom)
+    symptoms = models.ManyToManyField(Symptom, related_name='diseases')
     description = models.TextField(blank=True)
+    info_link = models.URLField(blank=True, help_text="Link to learn more about this disease")
 
     def __str__(self):
         return self.name
