ad9ca0cf52a93b865c03f0aa141b8cf788de2627
Marina Kostova <116514172+marina9222@users.noreply.github.com>
2025-11-13 16:12:53 +0000

    Add: update disase model

--- checker/models.py
+++ checker/models.py
@@ -3,16 +3,20 @@ from django.db import models
 
 class Symptom(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    
 
     def __str__(self):
         return self.name
 
 
 class Disease(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    symptoms = models.ManyToManyField(Symptom, related_name='diseases')
-    description = models.TextField(blank=True)
-    info_link = models.URLField(blank=True, help_text="Link to learn more about this disease")
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    info_link = models.URLField(blank=True, null=True)
+    symptoms = models.ManyToManyField('Symptom', related_name='diseases')
+
+    
+
 
     def __str__(self):
         return self.name
