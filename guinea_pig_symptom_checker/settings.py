fd36b1ab7c10f1d3c547407e90ebd1a81db7502a
Marina Kostova <116514172+marina9222@users.noreply.github.com>
2025-11-19 16:02:18 +0000

    Fix: change the database from render to Neon

--- guinea_pig_symptom_checker/settings.py
+++ guinea_pig_symptom_checker/settings.py
@@ -78,9 +78,16 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-     'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
+
 
 
 # Password validation
